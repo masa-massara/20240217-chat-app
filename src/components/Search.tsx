8e37ad703a1a0892f9c6f06487c650b1b8f0247c
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-02-17 05:40:20 +0900

    検索箇所の雛形作成

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="友達を検索"/>
+      </div>
+      <div className="userChat">
+        <img src="https://ih1.redbubble.net/image.1259577480.8618/st,small,507x507-pad,600x600,f8f8f8.jpg" alt="" />
+        <div className="userChatInfo">
+          <span>ジュン</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
