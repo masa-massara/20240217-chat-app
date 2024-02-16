9d72638b0e3b0ebe04c71c2347254e8fed42ac4f
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-02-17 02:23:45 +0900

    ログイン画面の作成

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import Add from "../images/addAvatar.png";
 
-export const Login = () => {
-  return <div>Login</div>;
+const Login = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Line?</span>
+        <span className="title">登録</span>
+        <form action="">
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="Email" />
+          
+          <button>Sign in</button>
+        </form>
+        <p>
+          アカウントを持っていませんか？ <span className="login">登録</span>
+        </p>
+      </div>
+    </div>
+  );
 };
+
+export default Login;
