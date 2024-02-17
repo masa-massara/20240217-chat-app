6387a7b2348f15bc75e82b625da0fd870e853bb3
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-02-18 04:08:50 +0900

    Chatコンポーネントの雛形完成

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Chats = () => {
   return (
-    <div>Chats</div>
-  )
-}
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://ih1.redbubble.net/image.1259577480.8618/st,small,507x507-pad,600x600,f8f8f8.jpg"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>ジュン</span>
+          <p>こんにちは</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chats
+export default Chats;
