9e4a0444a828ece3efae7c4625f48bbdf918cb5b
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-02-18 14:55:24 +0900

    文章の入力コンポーネントの雛形完成

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Img from "../images/img.png"
+import Attach from "../images/attach.png"
+
 
 const Input = () => {
   return (
-    <div>Input</div>
-  )
-}
+    <div className="input">
+      <input type="text" placeholder="文章を入力" />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input type="file" style={{ display: "none" }} id ="file"/>
+        <label htmlFor="file">
+          <img src={Img} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
+};
 
-export default Input
+export default Input;
