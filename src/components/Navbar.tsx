34107b2ad932441de5530bf9457320023e4c1409
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-02-25 05:42:02 +0900

    signOutを追加

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,24 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { auth } from "../firebase";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <span className="logo">Line?</span>
       <div className="user">
-        <img src="https://ih1.redbubble.net/image.1259577480.8618/st,small,507x507-pad,600x600,f8f8f8.jpg" alt=""  />
+        <img
+          src="https://ih1.redbubble.net/image.1259577480.8618/st,small,507x507-pad,600x600,f8f8f8.jpg"
+          alt=""
+        />
         <span>ペーター</span>
-        <button>ログアウト</button>
+        <button
+          onClick={() => {
+            signOut(auth);
+          }}
+        >
+          ログアウト
+        </button>
       </div>
     </div>
   );
