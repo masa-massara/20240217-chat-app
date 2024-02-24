16d00fc383bb962af8ff7925793adaf0edc3960f
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-02-24 23:46:19 +0900

    react-router-domのインポート文を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import Register from './pages/Register';
 import "./style.scss";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 import Home from './pages/Home';
 
 function App() {
