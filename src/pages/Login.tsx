2b78a9bdda00a436f6dc8154a60f52cc1be23183
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-02-25 20:27:40 +0900

    ログイン機能の実装

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate,Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error) {
+      setErr(true);
+    }
+  };
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Line?</span>
         <span className="title">登録</span>
-        <form action="">
-          <input type="text" placeholder="Username" />
+        <form onSubmit={handleSubmit}>
           <input type="email" placeholder="Email" />
-          
-          <button>Sign in</button>
+          <input type="password" placeholder="Password" />
+
+          <button>Log in</button>
+          {err && <span>エラーが発生しました</span>}
         </form>
         <p>
-          アカウントを持っていませんか？ <span className="login">登録</span>
+          アカウントを持っていませんか？ <span className="signin"><Link to="/register">登録</Link></span>
         </p>
       </div>
     </div>
