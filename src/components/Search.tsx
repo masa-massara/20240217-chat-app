ccf0120f37aa4bb6e53b18ecd3cd62dc707f20c8
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-02-28 22:50:29 +0900

    useStateの追加

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="友達を検索"/>
+        <input type="text" placeholder="友達を検索" />
       </div>
       <div className="userChat">
-        <img src="https://ih1.redbubble.net/image.1259577480.8618/st,small,507x507-pad,600x600,f8f8f8.jpg" alt="" />
+        <img
+          src="https://ih1.redbubble.net/image.1259577480.8618/st,small,507x507-pad,600x600,f8f8f8.jpg"
+          alt=""
+        />
         <div className="userChatInfo">
           <span>ジュン</span>
         </div>
